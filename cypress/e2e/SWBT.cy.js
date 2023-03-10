ceba74b9923a9caffb0975efe914eb507a2157c9
sairelcabahaga05 <scabahaga@ssct.edu.ph>
2023-03-10 23:53:22 +0800

    asdasd

--- cypress/e2e/SWBT.cy.js
+++ cypress/e2e/SWBT.cy.js
@@ -1,41 +1,55 @@
 
-const timeOut= {timeout:100000}
+const timeOut= {timeout:1000000}
 
-describe('SWBT Testing', ()=>{
+describe('Creating SWBT New User', ()=>{
 
 
-it('Login with valid all',()=>{
+it('Login',()=>{
 
     cy.visit('https://swbt-43857.alpha.reportheld-saas.com/admin/main.html',timeOut)
-    cy.get('.login-logo',timeOut).should('be.visible')
+    //cy.get('.login-logo',timeOut).should('be.visible')
         
+    //Enter Valid Username
     cy.get('#username',timeOut)
         .should('be.visible')
         .type('swbt-it-admin')
 
-
+    //Enter Valid Password
     cy.get('#password',timeOut)
         .should('be.visible')
         .type('testOJT**123')
 
+    //Click Login
     cy.get('#login',timeOut)
         .should('be.visible')
         .click()
+
+    //Checking if its Loading
+    cy.get('.wrapper > .inner',timeOut)
+        .should('be.visible')
+        .and('contain','Please be patient while data is loaded completely',timeOut)
+
     })
+
+
+
+it('Go to Users',()=>{
+
+    cy.get('.modalHost',timeOut)
+        .should('be.visible')
+        .click()
+
+})
 
 /*
-it('Login with invalid pass',()=>{
+it('Create a New User',()=>{
 
-    cy.visit('https://swbt-43857.alpha.reportheld-saas.com/admin/main.html')
-    
-    
-    })
+    cy.get('.modalHost',timeOut)
+        .should('be.visible')
+        .click()
 
- it('Login with invalid pass',()=>{
-
-     cy.visit('https://swbt-43857.alpha.reportheld-saas.com/admin/main.html')
-        
-        
-     }) 
+})
 */
+
+
 })
