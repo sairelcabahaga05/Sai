80eb53c4b0bf6d619dfd9a0d6fe75d2a358748b7
unknown <scabahaga@ssct.edu.ph>
2023-03-13 15:14:37 +0800

    kuyang FN & LN

--- cypress/e2e/SWBT.cy.js
+++ cypress/e2e/SWBT.cy.js
@@ -29,27 +29,74 @@ it('Login',()=>{
         .should('be.visible')
         .and('contain','Please be patient while data is loaded completely',timeOut)
 
-    })
+    
 
-
-
-it('Go to Users',()=>{
-
-    cy.get('.modalHost',timeOut)
+    cy.wait(200000) //waiting for the loading to be done
+    cy.get('.users',timeOut).click() //to click user icon
+       
+    //Input User Username
+    cy.get(':nth-child(2) > .recommended-input',timeOut)
         .should('be.visible')
-        .click()
+        .type('Sairel Cabahaga')
 
-})
+    //Input User Password
+    cy.get(':nth-child(3) > .recommended-input',timeOut)
+        .should('be.visible')
+        .type('12345678')
 
 /*
-it('Create a New User',()=>{
+    cy.get('//input[@data-bind="value: firstName"]',timeOut)
+        .should('be.visible')
+        .type('Sairel')
 
-    cy.get('.modalHost',timeOut)
+        
+    cy.get('//input[@data-bind="value: lastName"]',timeOut)
+        .should('be.visible')
+        .type('Cabahaga')
+*/
+
+    //Input User Birthdate
+    cy.get('[type="date"]',timeOut)
+        .should('be.visible')
+        .type('2001-07-12')
+    
+    //Input User Email Add
+    cy.get('[data-bind="value: email"]',timeOut)
+        .should('be.visible')
+        .type('scabahaga@ssct.edu.ph')
+
+    //Input User Contact Number
+    cy.get('[data-bind="value: phone"]')
+        .should('be.visible')
+        .type('09505001514')
+
+    //Choose User Primary Group
+    cy.get('.recommended-input-primary-group > .btn-group > .btn > .filter-option',timeOut).click()
+    cy.get('.recommended-input-primary-group > .btn-group > .open > .dropdown-menu > [data-original-index="1"] > a > .text').click()
+    
+    //Choose User Configuration Preset
+    cy.get(':nth-child(8) > .btn-group > .btn > .filter-option',timeOut).click()
+    cy.get(':nth-child(8) > .btn-group > .open > .dropdown-menu > .selected > a').click()
+    
+    //Choose Additional Group
+    cy.get('.item-with-add-button > .btn-group > .btn').click()
+    cy.get('.item-with-add-button > .btn-group > .open > .dropdown-menu > [data-original-index="2"] > a').click()
+    cy.get('.item-with-add-button > .groupxs-btn').click()
+
+    //To Save All Inputs
+    cy.contains('Save')
         .should('be.visible')
         .click()
+ 
+/*
+    //To Confirm Inputs
+    cy.get('.wrapper',timeOut)
+        .should('be.visible')
+        .and('contain','Are you sure you want to create new user?')
+
+*/
 
 })
-*/
 
 
 })
