c1e7f6895b32bddce64998b9b39f41bd1ed9cb08
Sairel Mae Cabahaga <scabahaga@ssct.edu.ph>
2023-03-29 06:03:57 +0800

    flender test

--- cypress/e2e/flender.cy.js
+++ cypress/e2e/flender.cy.js
@@ -1,60 +1,70 @@
-const timeOut = {timeout: 1000000}
+const timeOut = {
+    timeout: 1000000
+}
 
 //beforeEach(function(){cy.visit('https://flender.beta.reportheld.com/office/main.html?lng=en#sites/false',timeOut)})
 
-describe('Create Protocol', function(){
+describe('Create Protocol', function () {
 
-    it ('Login with Valid Credentials', function(){
+    it('Login with Valid Credentials', function () {
 
-        cy.visit('https://flender.beta.reportheld.com/office/main.html?lng=en#sites/false',timeOut)
+        cy.visit('https://flender.beta.reportheld.com/office/main.html?lng=en#sites/false', timeOut)
 
-        cy.get('#username',timeOut).should('be.visible').type('user3') //Inputing Valid Username 
-        cy.get('#password',timeOut).should('be.visible').type('123') //Inputing Valid Password
+        cy.get('#username', timeOut).should('be.visible').type('t-sairel') //Inputing Valid Username 
+        cy.get('#password', timeOut).should('be.visible').type('asdfasdf') //Inputing Valid Password
         cy.get('#login').should('be.visible').click()
 
     })
 
-    it ('Login with Invalid Credentials', function(){
+    it('Login with Invalid Credentials', function () {
 
-        cy.visit('https://flender.beta.reportheld.com/office/main.html?lng=en#sites/false',timeOut)
+        cy.visit('https://flender.beta.reportheld.com/office/main.html?lng=en#sites/false', timeOut)
 
-        cy.get('#username',timeOut).should('be.visible').type('user123') //Inputing Invalid Username 
-        cy.get('#password',timeOut).should('be.visible').type('00000') //Inputing Invalid Username 
+        cy.get('#username', timeOut).should('be.visible').type('user123') //Inputing Invalid Username 
+        cy.get('#password', timeOut).should('be.visible').type('00000') //Inputing Invalid Username 
         cy.get('#login').should('be.visible').click()
         cy.get('.toast-message').should('contain', 'Access denied') //Error Notification Should Exist
 
     })
 
 
-    it.only('Create Protocol', function(){
+    it.only('Create Protocol', function () {
 
-        cy.visit('https://flender.beta.reportheld.com/office/main.html?lng=en#sites/false',timeOut)
+        cy.visit('https://flender.beta.reportheld.com/office/main.html?lng=en#sites/false', timeOut)
 
-        cy.get('#username',timeOut).should('be.visible').type('user3')
-        cy.get('#password',timeOut).should('be.visible').type('123')
+        cy.get('#username', timeOut).should('be.visible').type('t-sairel')
+        cy.get('#password', timeOut).should('be.visible').type('asdfasdf')
         cy.get('#login').should('be.visible').click()
+        // cy.get('.wrapper > .inner').should('be.visible').should('contain', 'Please be patient while data is loaded completely')
 
 
-        //cy.get('.wrapper > .inner').should('be.visible').should('contain', 'Please be patient')
-        cy.wait(200000)
-        cy.get('.scrollable > .protocols',timeOut).should('be.visible').click() // To click the protocol icon
+        cy.wait(80000)
+        cy.get('.scrollable > .protocols', timeOut).should('be.visible').click() // To click the protocol icon
+        cy.contains('Create protocol', timeOut).should('be.visible').click() // To click the protocol icon
 
-        cy.contains('Create protocol',timeOut).should('be.visible').click() // To click the protocol icon
+        cy.wait(5000)
+        cy.get('.button-container.left').first().within(() => {
+            cy.get('button').click()
+            cy.contains('FLENDER').click()
+        })
+        cy.wait(5000)
+        cy.get('.button-container.right').first().within(() => {
+            cy.get('button').click()
+            cy.contains('Voerde').click()
+        })
+        cy.contains('Ok', timeOut).should('be.visible').click()
 
-        //cy.get('.right > .btn-group > .open > .dropdown-menu > [data-original-index="1"] > a',timeOut).should('be.visible').click()
-        //cy.contains('FLENDER',timeOut).should('be.visible').click()
+        cy.get('div[class="inner clearfix"]', timeOut).within(() => {
+            cy.contains("Are you sure you want to create a new protocol briefcase?").should('be.visible') 
+            cy.contains('Ok', timeOut).should('be.visible').click()
+        })
 
-        //cy.get('select-powerplant-popup > base-popup > [data-bind="visible: showPopup, closeOnClick: showPopup"] > .popup > .wrapper > .alternative-contacts > .container-fluid > :nth-child(1) > .right > .btn-group > .open > .dropdown-menu > [data-original-index="0"] > a',timeOut).should('be.visible').click()
-        //cy.get('select-powerplant-popup > base-popup > [data-bind="visible: showPopup, closeOnClick: showPopup"] > .popup > .wrapper > .alternative-contacts > .container-fluid > :nth-child(1) > .right > .btn-group > .open > .dropdown-menu > .selected > a',timeOut).should('be.visible').click()
+        cy.get('div[class="inner clearfix"]', timeOut).within(() => {
+            cy.contains("Briefcase name").should('be.visible') 
+            cy.get('.black-text-input').should('be.visible').should('include.value', 'Befundbericht')
+            cy.contains('Ok', timeOut).should('be.visible').click()
+        })
 
-        cy.get('div[class="button-container left"] button[title="FLENDER"]',timeOut).should('be.visible').click()
-        cy.get('button[title="Voerde"]',timeOut).should('be.visible').click()
-        cy.contains('Ok',timeOut).should('be.visible').click()
-
-        cy.get('div[class="inner clearfix"]',timeOut).should('contain',"Are you sure you want to create a new protocol briefcase?")
-        cy.contains('Ok',timeOut).should('be.visible').click()
-
-        
     })
 
 
